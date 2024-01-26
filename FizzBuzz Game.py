f92b68fb2104c212a5111f5f4a56881253710e59
ZubairMustafa5167 <zubair.m5167@gmail.com>
2024-01-26 23:03:02 +0500

    FizzBuzz Game

---
+++ FizzBuzz Game.py
@@ -0,0 +1,10 @@
+# FizzBuzz Game
+for i in range(1,50,1):
+    if i % 3 == 0 and i % 5 == 0:
+        print(i,",","FizzBuzz")
+    elif i % 3 == 0 :
+        print(i,",","Fizz")
+    elif i % 5 == 0 :
+        print(i,",","Buzz")
+    else:
+        print(i)

b4ab8e9e05857273653e7acbd2ecfa412a94436b
ZubairMustafa5167 <zubair.m5167@gmail.com>
2024-01-26 22:59:54 +0500

    Calculator

---
+++ Calculator .py
@@ -0,0 +1,11 @@
+a=float(input("Enter First Number :"))
+opr=input("Enter operator + - * / :")
+b=float(input("Enter Second numberb :"))
+if opr== "+" :
+    print(a+b)
+elif opr=="-":
+    print(a-b)
+elif opr=="*":
+    print(a*b)
+elif opr=="/":
+    print(a/b)

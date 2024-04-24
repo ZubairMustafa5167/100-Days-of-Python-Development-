546713023ac82f3ef730ba78677c44450c9a532a
ZubairMustafa5167 <zubair.m5167@gmail.com>
2024-04-24 11:06:53 +0500

    Create Factorial Of Number.py

---
+++ Factorial Of Number.py
@@ -0,0 +1,10 @@
+# Factorial Of Number 
+#user value for Factorial 
+num = int(input("Enter Number : "))
+f=1
+count=1
+#count the number 
+while count <= num :
+    f=f*count
+    count += 1
+print(f)
